5e7bea5815bb5495fe6f09262d1bde06cf5ff160
JadonF <jadon.fournier@ocsbstudent.ca>
2018-03-08 10:20:35 -0500

    Initial commit

---
+++ RockPaperScissorsForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RockPaperScissors
+{
+    partial class frmRockPaperScissors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblChoice = new System.Windows.Forms.Label();
+            this.radRock = new System.Windows.Forms.RadioButton();
+            this.radPaper = new System.Windows.Forms.RadioButton();
+            this.radScissors = new System.Windows.Forms.RadioButton();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblComChoice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblChoice
+            // 
+            this.lblChoice.AutoSize = true;
+            this.lblChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblChoice.Location = new System.Drawing.Point(104, 30);
+            this.lblChoice.Name = "lblChoice";
+            this.lblChoice.Size = new System.Drawing.Size(333, 26);
+            this.lblChoice.TabIndex = 0;
+            this.lblChoice.Text = "Choose Rock, Paper or Scissors!";
+            // 
+            // radRock
+            // 
+            this.radRock.AutoSize = true;
+            this.radRock.Location = new System.Drawing.Point(108, 100);
+            this.radRock.Name = "radRock";
+            this.radRock.Size = new System.Drawing.Size(51, 17);
+            this.radRock.TabIndex = 1;
+            this.radRock.TabStop = true;
+            this.radRock.Text = "Rock";
+            this.radRock.UseVisualStyleBackColor = true;
+            this.radRock.CheckedChanged += new System.EventHandler(this.radRock_CheckedChanged);
+            // 
+            // radPaper
+            // 
+            this.radPaper.AutoSize = true;
+            this.radPaper.Location = new System.Drawing.Point(236, 100);
+            this.radPaper.Name = "radPaper";
+            this.radPaper.Size = new System.Drawing.Size(53, 17);
+            this.radPaper.TabIndex = 2;
+            this.radPaper.TabStop = true;
+            this.radPaper.Text = "Paper";
+            this.radPaper.UseVisualStyleBackColor = true;
+            this.radPaper.CheckedChanged += new System.EventHandler(this.radPaper_CheckedChanged);
+            // 
+            // radScissors
+            // 
+            this.radScissors.AutoSize = true;
+            this.radScissors.Location = new System.Drawing.Point(372, 100);
+            this.radScissors.Name = "radScissors";
+            this.radScissors.Size = new System.Drawing.Size(64, 17);
+            this.radScissors.TabIndex = 3;
+            this.radScissors.TabStop = true;
+            this.radScissors.Text = "Scissors";
+            this.radScissors.UseVisualStyleBackColor = true;
+            this.radScissors.CheckedChanged += new System.EventHandler(this.radScissors_CheckedChanged);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnPlay.Location = new System.Drawing.Point(224, 144);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 35);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAnswer.Location = new System.Drawing.Point(104, 233);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(85, 26);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // lblComChoice
+            // 
+            this.lblComChoice.AutoSize = true;
+            this.lblComChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblComChoice.Location = new System.Drawing.Point(337, 233);
+            this.lblComChoice.Name = "lblComChoice";
+            this.lblComChoice.Size = new System.Drawing.Size(127, 26);
+            this.lblComChoice.TabIndex = 6;
+            this.lblComChoice.Text = "ComChoice";
+            // 
+            // frmRockPaperScissors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 319);
+            this.Controls.Add(this.lblComChoice);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.radScissors);
+            this.Controls.Add(this.radPaper);
+            this.Controls.Add(this.radRock);
+            this.Controls.Add(this.lblChoice);
+            this.Name = "frmRockPaperScissors";
+            this.Text = "Rock, Paper, Scissors!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblChoice;
+        private System.Windows.Forms.RadioButton radRock;
+        private System.Windows.Forms.RadioButton radPaper;
+        private System.Windows.Forms.RadioButton radScissors;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblComChoice;
+    }
+}
+
---
+++ RockPaperScissorsForm.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 8-Mar-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #18 - Rock, Paper, Scissors
+ * This program allows you to play rock paper scissors against a a computer
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class frmRockPaperScissors : Form
+    {
+
+        //Define variables and constants
+        int PlayerChoice = 0;
+        int ComChoice = 0;
+        Random comRandom;
+        const int MIN = 1;
+        const int MAX = 3;
+
+        public frmRockPaperScissors()
+        {
+            InitializeComponent();
+            comRandom = new Random();
+
+            //Hide labels
+            this.lblAnswer.Hide();
+            this.lblComChoice.Hide();
+        }
+
+        private void radRock_CheckedChanged(object sender, EventArgs e)
+        {
+            PlayerChoice = 1;
+        }
+
+        private void radPaper_CheckedChanged(object sender, EventArgs e)
+        {
+            PlayerChoice = 2;
+        }
+
+        private void radScissors_CheckedChanged(object sender, EventArgs e)
+        {
+            PlayerChoice = 3;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //Randomly generate the computers choice
+            ComChoice = comRandom.Next(MIN, MAX + 1);
+
+            //Show the computers descision
+            if (ComChoice == 1)
+            {
+                lblComChoice.Text = "Rock.";
+            }
+
+            else if (ComChoice == 2)
+            {
+                lblComChoice.Text = "Paper.";
+            }
+
+            else
+            {
+                lblComChoice.Text = "Scissors.";
+            }
+
+            //Check the variables
+            if (PlayerChoice == 1)
+            {
+                if (ComChoice == 1)
+                {
+                    lblAnswer.Text = "Tie.";
+                }
+
+                else if (ComChoice == 2)
+                {
+                    lblAnswer.Text = "The computer wins.";
+                }
+
+                else
+                {
+                    lblAnswer.Text = "You Win!";
+                }
+            }
+
+            if (PlayerChoice == 2)
+            {
+                if (ComChoice == 2)
+                {
+                    lblAnswer.Text = "Tie.";
+                }
+
+                else if (ComChoice == 3)
+                {
+                    lblAnswer.Text = "The computer wins.";
+                }
+
+                else
+                {
+                    lblAnswer.Text = "You Win!";
+                }
+            }
+
+            if (PlayerChoice == 3)
+            {
+                if (ComChoice == 3)
+                {
+                    lblAnswer.Text = "Tie.";
+                }
+
+                else if (ComChoice == 1)
+                {
+                    lblAnswer.Text = "The computer wins.";
+                }
+
+                else
+                {
+                    lblAnswer.Text = "You Win!";
+                }
+            }
+
+            //Show labels
+            this.lblAnswer.Show();
+            this.lblComChoice.Show();
+
+        }
+    }
+}
